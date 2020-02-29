ae595a0aec1b7bebfd7e1bce36c265ad8509abc7
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-02-29 21:30:12 +0100

    added serverconfig and its init

---
+++ init/10_serverconfigs.php
@@ -0,0 +1,25 @@
+<?
+date_default_timezone_set('Europe/Berlin');        //servers timezone
+
+/* CSGO CONFIG */
+//TODO
+/*
+$csgoConfig['server_name'] = "My server name";                      //Name of your server
+$csgoConfig['admin'] = "Admins name";                               //Your name / IGN (just changes color in console for the admin name)
+$csgoConfig['server_dir'] = "server\\";                             //Directory of your Minecraft server files.
+$csgoConfig['server_log'] = "logs\\latest.log";                     //location of current server log inside server dir
+$csgoConfig['username'] = "username";                               //Admin username (note: case sensitive)
+$csgoConfig['password'] = "password";                               //Admin password (note: case sensitive)
+$csgoConfig['pmax'] = 200;
+*/
+/* MINECRAFT CONFIG */
+
+$minecraftConfig['serverName'] = "My server name";                  //Name of your server
+$minecraftConfig['admin'] = "Admins name";                          //Your name / IGN (just changes color in console for the admin name)
+$minecraftConfig['serverDir'] = "server\\";                         //Directory of your Minecraft server files.
+$minecraftConfig['serverLog'] = "logs\\latest.log";                 //location of current server log inside server dir
+$minecraftConfig['username'] = "username";                          //Admin username (note: case sensitive)
+$minecraftConfig['password'] = "password";                          //Admin password (note: case sensitive)
+$minecraftConfig['pmax'] = 200;
+
+/* DISCORD CONFIG */

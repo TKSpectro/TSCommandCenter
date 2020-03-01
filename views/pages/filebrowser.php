519efde5fbb823d5936d9070fd82018e403693c5
z3ttee <cedric.zitzmann@gmail.com>
2020-03-01 20:43:19 +0100

    Added FTP

--- views/pages/filebrowser.php
+++ views/pages/filebrowser.php
@@ -1,12 +1,54 @@
 <?
-$ftpConnection = ftp_connect('serverts.zapto.org', 21, 90);
-if(ftp_login($ftpConnection, 'Server', 'TSCockila14') && ftp_chdir($ftpConnection,'/')) {
+try {
+    $ftpConnection = ftp_connect('serverts.zapto.org', 21, 3);
+    $loggedIn = ftp_login($ftpConnection, 'root', 'TSCockila14');
 
-    //$contents = ftp_mlsd($ftpConnection, ".");
-    $contents = ftp_rawlist($ftpConnection, '/');
-    //$contents = ftp_raw($ftpConnection, 'mlsd');
+    if($ftpConnection != null && $loggedIn && ftp_chdir($ftpConnection,'/')) {
+        $contents = ftp_mlsd($ftpConnection, "./webserver");
 
-    var_dump($contents);
-} else {
-    echo 'Es gab einen Fehler beim Anmelden über FTP.';
+        /*foreach($contents as $file) {
+            $name = $file['name'];
+            $type = $file['type'];
+            $modified = $file['modify'];
+
+            echo $name.'<br />';
+        }*/
+
+    } else {
+        echo 'Es gab einen Fehler beim Anmelden über FTP.';
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
+?>
+<div class="browser_path">
+    <i class="fas fa-home"></i> /
+</div>
+<div class="browser_content">
+    <table>
+        <tr>
+            <th id="col_icon"></th>
+            <th id="col_name">Dateiname</th>
+            <th id="col_modified">Geändert am</th>
+        </tr>
+
+        <?php
+            if($contents) {
+                foreach($contents as $file) {
+                    $name = $file['name'];
+                    $type = $file['type'];
+                    $modified = $file['modify'];
+        
+                    echo '
+                        <tr>
+                            <td class="col_icon">'.($type === 'dir' ? '<i class="far fa-folder"></i>' : '<i class="far fa-file-alt"></i>').'</td>
+                            <td>'.$name.'</td>
+                            <td></td>
+                        </tr>
+                    ';
+                }
+            }
+
+        ?>
+    </table>
+</div>

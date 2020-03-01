53142284fc9f2805a9731de26a79756f12eb5b8c
z3ttee <cedric.zitzmann@gmail.com>
2020-03-01 20:43:32 +0100

    Modified

--- controller/filebrowserController.php
+++ controller/filebrowserController.php
@@ -6,9 +6,6 @@ class FilebrowserController extends \app\core\Controller
     public function actionFilebrowser()
     {
         // ftp://username:password@subdomain.example.com/path1/path2/
-        
-
-
         $this->_params['title'] = 'Dateibrowser';
     }
 }

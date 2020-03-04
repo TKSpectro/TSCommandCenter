906a0b91503d5e034d0f7eec66143fa4e0c6af2e
z3ttee <cedric.zitzmann@gmail.com>
2020-03-04 15:02:31 +0100

    Added

---
+++ views/pages/whitelist.php
@@ -0,0 +1,27 @@
+<?php
+var_dump(Whitelist::get());
+?>
+
+<div class="grid_container">
+    <div class="grid_item whitelist_item">
+        <img class="whitelist_item_picture" />
+        <div class="whitelist_item_description">
+            <h5>zettee#0899</h5>
+            <span class="whitelist_item_rank">Administrator</span>
+        </div>
+    </div>
+    <div class="grid_item whitelist_item">
+        <img class="whitelist_item_picture" />
+        <div class="whitelist_item_description">
+            <h5>zettee#0899</h5>
+            <span class="whitelist_item_rank">Administrator</span>
+        </div>
+    </div>
+    <div class="grid_item whitelist_item">
+        <img class="whitelist_item_picture" />
+        <div class="whitelist_item_description">
+            <h5>zettee#0899</h5>
+            <span class="whitelist_item_rank">Administrator</span>
+        </div>
+    </div>
+</div>
